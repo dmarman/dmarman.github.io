36b68956da7b29065282ba17fabc14de09ab98b4
Domingo <dmm.martin@gmail.com>
2020-08-27 00:58:31 +0200

    more models

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Pickr from '@simonwep/pickr'
 import '@simonwep/pickr/dist/themes/nano.min.css'
 import model from './model'
 import nameModel from './nameModel'
+import nextModel from './nextModel'
 import Mustache from 'mustache'
 import stub from './code.stub.html'
 
@@ -16,7 +17,7 @@ const pickrButton = Pickr.create({
   el: '.pickr',
   theme: 'nano', // or 'monolith', or 'nano'
 
-  default: '#2961B1',
+  default: '#1D19A9',
   comparison: false,
   showAlways: true,
   container: '.pickr-ctrl',
@@ -36,14 +37,41 @@ const pickrButton = Pickr.create({
   }
 });
 
-let output = model('#376AB4');
+let output = model('#1D19A9');
 draw(output);
+
+let next = nextModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))
+let output1 = model(rgbToHex(next.r1*255, next.g1*255,next.b1*255))
+let output2 = model(rgbToHex(next.r2*255, next.g2*255,next.b2*255))
+let output3 = model(rgbToHex(next.r3*255, next.g3*255,next.b3*255))
+
+paint(output1 , 1)
+paint(output2 , 2)
+paint(output3 , 3)
+name(output , '')
+name(output1 , 1)
+name(output2 , 2)
+name(output3 , 3)
 
 pickrButton.on('change', instance => {
   output = model(instance.toHEXA().toString())
   draw(output, document.querySelector('.name').value);
+
+  next = nextModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))
+  output1 = model(rgbToHex(next.r1*255, next.g1*255,next.b1*255))
+  output2 = model(rgbToHex(next.r2*255, next.g2*255,next.b2*255))
+  output3 = model(rgbToHex(next.r3*255, next.g3*255,next.b3*255))
+  paint(output1 , 1)
+  paint(output2 , 2)
+  paint(output3 , 3)
 });
 
+pickrButton.on('changestop', instance => {
+  name(output , '')
+  name(output1 , 1)
+  name(output2 , 2)
+  name(output3 , 3)
+});
 
 document.querySelector('.name').addEventListener('input', (e) => {
   let name = document.querySelector('.name').value;
@@ -51,38 +79,48 @@ document.querySelector('.name').addEventListener('input', (e) => {
   draw(output, name)
 });
 
+function paint(output, i = '')
+{
+  document.getElementById('output' + i + '0.5').style.backgroundColor = 'rgb(' + output['r0.5']*255 + ',' + output['g0.5']*255 + ',' + output['b0.5']*255 + ')';
+  document.getElementById('output' + i + '1').style.backgroundColor = 'rgb(' + output.r1*255 + ',' + output.g1*255 + ',' + output.b1*255 + ')';
+  document.getElementById('output' + i + '2').style.backgroundColor = 'rgb(' + output.r2*255 + ',' + output.g2*255 + ',' + output.b2*255 + ')';
+  document.getElementById('output' + i + '3').style.backgroundColor = 'rgb(' + output.r3*255 + ',' + output.g3*255 + ',' + output.b3*255 + ')';
+  document.getElementById('output' + i + '4').style.backgroundColor = 'rgb(' + output.r4*255 + ',' + output.g4*255 + ',' + output.b4*255 + ')';
+  document.getElementById('output' + i + '5').style.backgroundColor = 'rgb(' + output.r5*255 + ',' + output.g5*255 + ',' + output.b5*255 + ')';
+  document.getElementById('output' + i + '6').style.backgroundColor = 'rgb(' + output.r6*255 + ',' + output.g6*255 + ',' + output.b6*255 + ')';
+  document.getElementById('output' + i + '7').style.backgroundColor = 'rgb(' + output.r7*255 + ',' + output.g7*255 + ',' + output.b7*255 + ')';
+  document.getElementById('output' + i + '8').style.backgroundColor = 'rgb(' + output.r8*255 + ',' + output.g8*255 + ',' + output.b8*255 + ')';
+  document.getElementById('output' + i + '9').style.backgroundColor = 'rgb(' + output.r9*255 + ',' + output.g9*255 + ',' + output.b9*255 + ')';
+  document.getElementById('text' + i + '0.5').style.color = 'rgb(' + output['r0.5']*255 + ',' + output['g0.5']*255 + ',' + output['b0.5']*255 + ')';
+  document.getElementById('text' + i + '1').style.color = 'rgb(' + output.r1*255 + ',' + output.g1*255 + ',' + output.b1*255 + ')';
+  document.getElementById('text' + i + '2').style.color = 'rgb(' + output.r2*255 + ',' + output.g2*255 + ',' + output.b2*255 + ')';
+  document.getElementById('text' + i + '3').style.color = 'rgb(' + output.r3*255 + ',' + output.g3*255 + ',' + output.b3*255 + ')';
+  document.getElementById('text' + i + '4').style.color = 'rgb(' + output.r4*255 + ',' + output.g4*255 + ',' + output.b4*255 + ')';
+  document.getElementById('text' + i + '5').style.color = 'rgb(' + output.r5*255 + ',' + output.g5*255 + ',' + output.b5*255 + ')';
+  document.getElementById('text' + i + '6').style.color = 'rgb(' + output.r6*255 + ',' + output.g6*255 + ',' + output.b6*255 + ')';
+  document.getElementById('text' + i + '7').style.color = 'rgb(' + output.r7*255 + ',' + output.g7*255 + ',' + output.b7*255 + ')';
+  document.getElementById('text' + i + '8').style.color = 'rgb(' + output.r8*255 + ',' + output.g8*255 + ',' + output.b8*255 + ')';
+  document.getElementById('text' + i + '9').style.color = 'rgb(' + output.r9*255 + ',' + output.g9*255 + ',' + output.b9*255 + ')';
+  document.getElementById('hex' + i + '0.5').innerHTML = rgbToHex(output['r0.5']*255, output['g0.5']*255, output['b0.5']*255);
+  document.getElementById('hex' + i + '1').innerHTML = rgbToHex(output.r1*255, output.g1*255, output.b1*255);
+  document.getElementById('hex' + i + '2').innerHTML = rgbToHex(output.r2*255, output.g2*255, output.b2*255);
+  document.getElementById('hex' + i + '3').innerHTML = rgbToHex(output.r3*255, output.g3*255, output.b3*255);
+  document.getElementById('hex' + i + '4').innerHTML = rgbToHex(output.r4*255, output.g4*255, output.b4*255);
+  document.getElementById('hex' + i + '5').innerHTML = rgbToHex(output.r5*255, output.g5*255, output.b5*255);
+  document.getElementById('hex' + i + '6').innerHTML = rgbToHex(output.r6*255, output.g6*255, output.b6*255);
+  document.getElementById('hex' + i + '7').innerHTML = rgbToHex(output.r7*255, output.g7*255, output.b7*255);
+  document.getElementById('hex' + i + '8').innerHTML = rgbToHex(output.r8*255, output.g8*255, output.b8*255);
+  document.getElementById('hex' + i + '9').innerHTML = rgbToHex(output.r9*255, output.g9*255, output.b9*255);
+}
+
+function name(output, i)
+{
+  document.getElementById('name' + i).innerHTML = nameModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))[0][0]
+}
+
 function draw(output, name = 'valencia')
 {
-  document.getElementById('output0.5').style.backgroundColor = 'rgb(' + output['r0.5']*255 + ',' + output['g0.5']*255 + ',' + output['b0.5']*255 + ')';
-  document.getElementById('output1').style.backgroundColor = 'rgb(' + output.r1*255 + ',' + output.g1*255 + ',' + output.b1*255 + ')';
-  document.getElementById('output2').style.backgroundColor = 'rgb(' + output.r2*255 + ',' + output.g2*255 + ',' + output.b2*255 + ')';
-  document.getElementById('output3').style.backgroundColor = 'rgb(' + output.r3*255 + ',' + output.g3*255 + ',' + output.b3*255 + ')';
-  document.getElementById('output4').style.backgroundColor = 'rgb(' + output.r4*255 + ',' + output.g4*255 + ',' + output.b4*255 + ')';
-  document.getElementById('output5').style.backgroundColor = 'rgb(' + output.r5*255 + ',' + output.g5*255 + ',' + output.b5*255 + ')';
-  document.getElementById('output6').style.backgroundColor = 'rgb(' + output.r6*255 + ',' + output.g6*255 + ',' + output.b6*255 + ')';
-  document.getElementById('output7').style.backgroundColor = 'rgb(' + output.r7*255 + ',' + output.g7*255 + ',' + output.b7*255 + ')';
-  document.getElementById('output8').style.backgroundColor = 'rgb(' + output.r8*255 + ',' + output.g8*255 + ',' + output.b8*255 + ')';
-  document.getElementById('output9').style.backgroundColor = 'rgb(' + output.r9*255 + ',' + output.g9*255 + ',' + output.b9*255 + ')';
-  document.getElementById('text0.5').style.color = 'rgb(' + output['r0.5']*255 + ',' + output['g0.5']*255 + ',' + output['b0.5']*255 + ')';
-  document.getElementById('text1').style.color = 'rgb(' + output.r1*255 + ',' + output.g1*255 + ',' + output.b1*255 + ')';
-  document.getElementById('text2').style.color = 'rgb(' + output.r2*255 + ',' + output.g2*255 + ',' + output.b2*255 + ')';
-  document.getElementById('text3').style.color = 'rgb(' + output.r3*255 + ',' + output.g3*255 + ',' + output.b3*255 + ')';
-  document.getElementById('text4').style.color = 'rgb(' + output.r4*255 + ',' + output.g4*255 + ',' + output.b4*255 + ')';
-  document.getElementById('text5').style.color = 'rgb(' + output.r5*255 + ',' + output.g5*255 + ',' + output.b5*255 + ')';
-  document.getElementById('text6').style.color = 'rgb(' + output.r6*255 + ',' + output.g6*255 + ',' + output.b6*255 + ')';
-  document.getElementById('text7').style.color = 'rgb(' + output.r7*255 + ',' + output.g7*255 + ',' + output.b7*255 + ')';
-  document.getElementById('text8').style.color = 'rgb(' + output.r8*255 + ',' + output.g8*255 + ',' + output.b8*255 + ')';
-  document.getElementById('text9').style.color = 'rgb(' + output.r9*255 + ',' + output.g9*255 + ',' + output.b9*255 + ')';
-  document.getElementById('hex0.5').innerHTML = rgbToHex(output['r0.5']*255, output['g0.5']*255, output['b0.5']*255);
-  document.getElementById('hex1').innerHTML = rgbToHex(output.r1*255, output.g1*255, output.b1*255);
-  document.getElementById('hex2').innerHTML = rgbToHex(output.r2*255, output.g2*255, output.b2*255);
-  document.getElementById('hex3').innerHTML = rgbToHex(output.r3*255, output.g3*255, output.b3*255);
-  document.getElementById('hex4').innerHTML = rgbToHex(output.r4*255, output.g4*255, output.b4*255);
-  document.getElementById('hex5').innerHTML = rgbToHex(output.r5*255, output.g5*255, output.b5*255);
-  document.getElementById('hex6').innerHTML = rgbToHex(output.r6*255, output.g6*255, output.b6*255);
-  document.getElementById('hex7').innerHTML = rgbToHex(output.r7*255, output.g7*255, output.b7*255);
-  document.getElementById('hex8').innerHTML = rgbToHex(output.r8*255, output.g8*255, output.b8*255);
-  document.getElementById('hex9').innerHTML = rgbToHex(output.r9*255, output.g9*255, output.b9*255);
+  paint(output)
 
   let view = (output, name) => {
     return {
@@ -100,24 +138,24 @@ function draw(output, name = 'valencia')
     }
   };
 
-  let colors = [];
-  colors.push(nameModel(rgbToHex(output.r4*255, output.g4*255, output.b4*255))[0])
-  colors.push(nameModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))[0])
+  // let colors = [];
+  // //colors.push(nameModel(rgbToHex(output.r3*255, output.g3*255, output.b3*255))[0])
+  // //colors.push(nameModel(rgbToHex(output.r4*255, output.g4*255, output.b4*255))[0])
+  // colors.push(nameModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))[0])
+  // //colors.push(nameModel(rgbToHex(output.r6*255, output.g6*255, output.b6*255))[0])
+  //
+  // let sortable = [];
+  // for (let color in colors) {
+  //   sortable.push([colors[color]]);
+  // }
+  //
+  // sortable.sort(function(a, b) {
+  //   return b[1] - a[1];
+  // });
 
-  let sortable = [];
-  for (let color in colors) {
-    sortable.push([colors[color]]);
-  }
-
-  sortable.sort(function(a, b) {
-    return b[1] - a[1];
-  });
-
-  if(document.querySelector('.name').value === '') name = sortable[0][0][0];
+  if(document.querySelector('.name').value === '') name = nameModel(rgbToHex(output.r5*255, output.g5*255, output.b5*255))[0][0];
 
   document.querySelector('.code').innerHTML = Mustache.render(stub, view(output, name));
-
-
 }
 
 
